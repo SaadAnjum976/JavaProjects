f35eb9064937702fbb289a9df83eaabea92d98ee
saadanjum <saadanjum@localhost>
2023-11-05 18:03:09 -0600

    Initial commit

---
+++ EvenOdd/src/EvenOdd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		 System.out.println("Enter a number: ");
+		    Scanner sc = new Scanner(System.in);
+		    int number =Integer.parseInt(sc.nextLine());
+		    int x = number%2;
+		    
+		    if(x==0){
+		      System.out.println("The number is Even");
+		    }
+		    else{
+		      System.out.println("The number is Odd");
+		    }
+	}
+
+}
