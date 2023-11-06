889c6152f6472899b40045863847034eb1eab1ed
saadanjum <saadanjum@localhost>
2023-11-05 18:27:19 -0600

    Cleaned up code

--- EvenOdd/src/EvenOdd.java
+++ EvenOdd/src/EvenOdd.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class EvenOdd {
-
 	public static void main(String[] args) {
 		 System.out.println("Enter a number: ");
 		    Scanner sc = new Scanner(System.in);
@@ -15,5 +14,4 @@ public class EvenOdd {
 		      System.out.println("The number is Odd");
 		    }
 	}
-
 }
