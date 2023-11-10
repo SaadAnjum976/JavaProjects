4c5ff626c343c999c0130eef4d49cdf04a14f137
saadanjum <saadanjum@localhost>
2023-11-10 17:06:09 -0600

    Initial commit

---
+++ Multiplesof310to50/src/Multiplesof310to50.java
@@ -0,0 +1,13 @@
+
+public class Multiplesof310to50 {
+
+	public static void main(String[] args) {
+		
+		 for(int i=10;i<50;i++){
+		      if(i%3==0)
+		        System.out.println(i);
+		    }
+
+	}
+
+}
